b40a883739fc420fcd339fcc29e06af6cafd310e
Triston Herbst <tristonherbst97@gmail.com>
2021-01-07 00:29:14 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -51,8 +51,11 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
-      :additional_characters => []
+      
    }
+   epic_tragedy[:additional_characters] = []
+   epic_tragedy[:additional_characters] << {name: "Prince Escalus"}
+   epic_tragedy[:additional_characters] << {name: "Apothecary"}
 
    # Write code below that does the following programmatically:
    #
