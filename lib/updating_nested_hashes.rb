f35e7f93787d0770eb4445a25803b30880278a06
Triston Herbst <tristonherbst97@gmail.com>
2021-01-07 00:25:37 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,8 +22,7 @@ def update_status
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 epic_tragedy[:montague][:hero][:status] = "dead"
 
-  # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  # Code your solution here:
+  
 
 
 
@@ -52,6 +51,7 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
+      :additional_characters => []
    }
 
    # Write code below that does the following programmatically:
